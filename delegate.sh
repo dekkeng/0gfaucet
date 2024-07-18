3d92aafc305670559adc01a09d92a40efb15e918
dekkeng <dek_keng@hotmail.com>
2024-07-18 16:05:53 +0700

    Add wait

--- delegate.sh
+++ delegate.sh
@@ -2,5 +2,7 @@
 
 while read -r line; do 
     printf "y\n$line\n" | 0gchaind keys add wallet1 --eth --recover
+    sleep 2
     printf "y\n" | 0gchaind tx staking delegate $1 999900ua0gi --from wallet1 --chain-id zgtendermint_16600-2 --gas=auto --gas-adjustment=1.7
+    sleep 2
 done < seeds.txt
