13b3884692017c9d74c3baf18ae25a28c72d602b
dekkeng <dek_keng@hotmail.com>
2024-07-19 11:11:03 +0700

    Try

--- torauto.py
+++ torauto.py
@@ -46,7 +46,10 @@ class Tor:
                 self.wait(5)
 
     def getPos(self, file, conf = 0.95):
-        return pyautogui.locateCenterOnScreen('./sample/'+file+'.png', confidence = conf)
+        try:
+            return pyautogui.locateCenterOnScreen('./sample/'+file+'.png', confidence = conf)
+        except pyautogui.ImageNotFoundException:
+            self.log('Image Not Found')
 
     def getAllPos(self, file, conf = 0.7):
         return pyautogui.locateAllOnScreen('./sample/'+file+'.png', confidence = conf)
